91aaffe7b84ec0a5f2f0e936d1ea14c236b794c6
61911 <psp.11.jb@gmail.com>
2020-05-15 19:43:36 +0530

    new code is added

--- multiuser-master/userpanel/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Customer, Business, Profile
-
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Customer)
-admin.site.register(Business)
-admin.site.register(Profile)
--- multiuser-master/userpanel/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserpanelConfig(AppConfig):
-    name = 'userpanel'
-
-    def ready(self):
-        import userpanel.signals
